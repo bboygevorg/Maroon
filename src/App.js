f2b42993694594b5f8f946ff049328e2ff6a6d6c
Artur <grigorevich1993artur@gmail.com>
2022-05-08 18:56:22 +0400

    fix slider arrows

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import './assets/styles/styles.scss'
 import MainLayout from "./layout/mainLayout";
 import Catalog from "./containers/catalog/catalog";
 import About from "./containers/about/about";
 import Contact from "./containers/contact/contact";
 import MainContainer from "./containers/mainContainer/mainContainer";
-
+import './App.css';
+import './assets/styles/styles.scss'
 
 function App() {
   return (
