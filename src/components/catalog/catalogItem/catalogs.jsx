b2b9338f31605e8ca64a2b7acde58fe4096ccfc0
Gevorg <bboygevor@gmail.com>
2022-06-05 20:01:48 +0400

    -aGeneral update

---
+++ src/components/catalog/catalogItem/catalogs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './catalogItem.module.scss'
+import productImage from '../../../assets/img/catalog/high@1x.jpg'
+import CatalogItem from "../components/catalogItem";
+
+const CatalogList = () => {
+    return (
+        <>
+            <CatalogItem />
+        </>
+    )
+}
+
+export default CatalogList;
